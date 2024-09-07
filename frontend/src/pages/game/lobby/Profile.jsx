26b508143270e230f7036739b0d8d64cbb92bc9f
captainAyan <ayanchakraborty105@gmail.com>
2024-09-08 02:29:16 +0530

    feat: :sparkles: added profile page

--- frontend/src/pages/game/lobby/Profile.jsx
+++ frontend/src/pages/game/lobby/Profile.jsx
@@ -1,7 +1,25 @@
+import Avatar from "../../../components/Avatar";
+
+import useStore from "../../../store";
+
 export default function Profile() {
+  const { user } = useStore((state) => state);
+
   return (
     <div>
       <h2>Profile</h2>
+      <center>
+        <Avatar
+          width={15}
+          cell={9}
+          color={"#8692f7"}
+          seed={user?.id}
+          className="avatar"
+        />
+        <h1 style={{ margin: "20px 0px 0px 0px" }}>{user?.username}</h1>
+
+        <p style={{}}>{user?.id}</p>
+      </center>
     </div>
   );
 }
